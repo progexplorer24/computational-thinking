fb9423c096ad95527a13324135c8a8e9a7ad2a18
Progexplorer <progexplorer@gmail.com>
2022-04-19 23:12:04 +0200

    feat: add initial commit

---
+++ monte-carlo.jl
@@ -0,0 +1,35 @@
+### A Pluto.jl notebook ###
+# v0.17.2
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 7263c87a-c024-11ec-2c14-c96249c8dc7d
+md"# Monte Carlo Simulation Notebook"
+
+# ╔═╡ a6269538-7b7a-4f0e-8f56-0ba7a8557c02
+md"**TODO LIST** 
+- create card type
+- use random simulation
+- create card deck
+- create evaluateHand function."
+
+# ╔═╡ 3ba400b3-4644-4c01-a117-28f24b84b681
+
+
+# ╔═╡ 84c4a46e-d329-4261-99ac-aaf1ceda7ebd
+
+
+# ╔═╡ 73f06b77-14d2-4a3e-b3f5-405ea58fde0d
+
+
+# ╔═╡ 6670a8a3-da13-4a9c-9f50-4e3be8925edd
+
+
+# ╔═╡ Cell order:
+# ╟─7263c87a-c024-11ec-2c14-c96249c8dc7d
+# ╟─a6269538-7b7a-4f0e-8f56-0ba7a8557c02
+# ╠═3ba400b3-4644-4c01-a117-28f24b84b681
+# ╠═84c4a46e-d329-4261-99ac-aaf1ceda7ebd
+# ╠═73f06b77-14d2-4a3e-b3f5-405ea58fde0d
+# ╠═6670a8a3-da13-4a9c-9f50-4e3be8925edd
